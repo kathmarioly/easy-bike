cb7d89ab8609425c45376011b074e87d87c77b07
kathmarioly <katerinemarioly@gmail.com>
2017-06-19 12:54:44 -0400

    JavaScript: Function initialize our map

---
+++ assets/js/main.js
@@ -0,0 +1,10 @@
+/*La función initMap inicializara el mapa*/
+
+function initMap(){
+	var map = new google.maps.Map(document.getElementById("map");
+		zoom: 5,
+		center: {lat: -9.1191427. lng: -77.0349046},
+		mapTypeControl:false,
+		streetViewControl: false}
+		)};
+	
